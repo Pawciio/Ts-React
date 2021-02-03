c0fb5dc247da6ca3077e4c3004534c6ee814af39
Pawciio <choldapawel@gmail.com>
2021-02-03 12:39:10 +0100

    initial commit - based project

---
+++ data.js
@@ -0,0 +1,70 @@
+module.exports = function () {
+  return {
+    products: [
+      {
+        id: 1,
+        name: "Kajak",
+        category: "sporty wodne",
+        description: "łódka przeznaczona dla jednej osoby",
+        price: 275,
+      },
+      {
+        id: 2,
+        name: "Kamizelka ratunkowa",
+        category: "sporty wodne",
+        description: "chroni i daje uroku",
+        price: 48.95,
+      },
+      {
+        id: 3,
+        name: "Piłka",
+        category: "piłka nożna",
+        description: "zatwierdzone przez FIFA rozmiar i waga",
+        price: 19.5,
+      },
+      {
+        id: 4,
+        name: "Flagi narożne",
+        category: "piłka nożna",
+        description: "nadadzą twojemu boisku profesjonalny wygląd",
+        price: 34.95,
+      },
+      {
+        id: 5,
+        name: "Stadion",
+        category: "piłka nożna",
+        description: "składany station na 35 000 osób",
+        price: 7950,
+      },
+      {
+        id: 6,
+        name: "Czapka",
+        category: "szachy",
+        description: "zwiększa efektywność mózgu o 75%",
+        price: 16,
+      },
+      {
+        id: 7,
+        name: "Niestabilne krzesło",
+        category: "szachy",
+        description: "zmniejsza szanse przeciwnika",
+        price: 29.95,
+      },
+      {
+        id: 8,
+        name: "Ludzka szachownica",
+        category: "szachy",
+        description: "przyjemna gra dla całej rodziny",
+        price: 75,
+      },
+      {
+        id: 9,
+        name: "Błyszczący król",
+        category: "szachy",
+        description: "pokryty złotem i wysadzany diamentami król",
+        price: 1200,
+      },
+    ],
+    orders: [],
+  };
+};
